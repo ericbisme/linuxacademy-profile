db070f95cf792becae1ef5604820c1172be5b59e
Eric Bolinger <ericb@ericbisme.net>
2017-10-22 20:45:57 -0600

    correct port for puppetdb

--- manifests/puppetboard.pp
+++ manifests/puppetboard.pp
@@ -6,7 +6,7 @@ class profile::puppetboard {
     groups              => 'puppet',
     manage_virtualenv   => true,
     puppetdb_host       => 'puppet.ericbisme.io',
-    puppetdb_port       => '8081',
+    puppetdb_port       => '8080',
     puppetdb_key        => "${ssl_dir}/private_keys/${puppetboard_certname}.pem",
     puppetdb_ssl_verify => "${ssl_dir}/certs/ca.pem",
     puppetdb_cert       => "${ssl_dir}/certs/${puppetboard_certname}.pem",
