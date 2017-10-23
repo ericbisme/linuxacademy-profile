402466f9a2f7d781a05d48ccac4716db1f326981
Eric Bolinger <ericb@ericbisme.net>
2017-10-22 20:01:32 -0600

    apache parts for puppetboard

--- manifests/puppetboard.pp
+++ manifests/puppetboard.pp
@@ -1,6 +1,7 @@
 class profile::puppetboard {
   $ssl_dir = $::settings::ssldir
   $puppetboard_certname = $::certname
+
   class { 'puppetboard':
     groups              => 'puppet',
     manage_virtualenv   => true,
@@ -10,4 +11,23 @@ class profile::puppetboard {
     puppetdb_ssl_verify => "${ssl_dir}/certs/ca.pem",
     puppetdb_cert       => "${ssl_dir}/certs/${puppetboard_certname}.pem",
   }
+
+	# Configure Apache
+	# Ensure it does *not* purge configuration files
+	class { 'apache':
+		purge_configs => false,
+		mpm_module    => 'prefork',
+		default_vhost => true,
+		default_mods  => false,
+	}
+
+	class { 'apache::mod::wsgi':
+     wsgi_socket_prefix => "/var/run/wsgi",
+  }
+
+	# Configure Puppetboard
+	class { 'puppetboard': }
+
+	# Access Puppetboard from example.com/puppetboard
+	class { 'puppetboard::apache::conf': }
 }
